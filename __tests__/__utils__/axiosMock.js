125c6f0ef8cfd3063755dca72a3fec959964c3cc
Matthew Black <matthewallenblack@gmail.com>
2023-10-13 16:30:16 -0500

    implement initial test suite and make necessary tweaks to instructions and starter files

---
+++ __tests__/__utils__/axiosMock.js
@@ -0,0 +1,57 @@
+const briefPause = require('./briefPause.js')
+
+
+// Mock the axios function:
+let axios = jest.fn(async (axiosArgument) => {
+  const requestMethod = axiosArgument.method.toUpperCase()
+  
+  // Take conditional action based on request method:
+  if (requestMethod === 'POST') {
+    const { numOne, numTwo, operator } = axiosArgument.data
+    const calculation = {
+      numOne: Number(numOne),
+      numTwo: Number(numTwo),
+      operator
+    }
+    
+    switch (operator) {
+      case '+':
+        calculation.result = calculation.numOne + calculation.numTwo;
+        break;
+      case '-':
+        calculation.result = calculation.numOne - calculation.numTwo;
+        break;
+      case '*':
+        calculation.result = calculation.numOne * calculation.numTwo;
+        break;
+      case '/':
+        calculation.result = calculation.numOne / calculation.numTwo;
+        break;
+      default:
+        break
+    }
+    
+    axios.testData.push(calculation)
+
+    await briefPause(150)
+
+    return Promise.resolve({
+      axiosWasCalledWith: axiosArgument,
+      status: 201,
+      requestMethod,
+    })
+  } else if (requestMethod === 'GET') {
+    await briefPause(10)
+
+    return Promise.resolve({
+      axiosWasCalledWith: axiosArgument,
+      status: 200,
+      data: axios.testData,
+      requestMethod,
+    })
+  }
+})
+
+axios.testData = []
+
+module.exports = axios
